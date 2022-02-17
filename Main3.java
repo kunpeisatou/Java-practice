88b01680e8cfc0d296da0e3fb848536ef91a5f03
kunpeisatou <kunpei0411@gmail.com>
2022-02-17 02:06:57 +0000

    220217

---
+++ Main3.java
@@ -0,0 +1,20 @@
+// Main3_0.java
+class Main {
+  public static void main(String[] args) {
+    System.out.println("===== メソッドの定義 =====");
+    // printDataメソッドを呼び出してください
+    printData();
+    
+  }
+  
+  
+  
+  
+  
+  
+  // printDataメソッドを定義してください
+  public static void printData(){
+    System.out.println("私の名前はKate Jonesです");
+  }
+}
+  
